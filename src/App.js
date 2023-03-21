3dd1e340aadd56fb959e59b30d0beffe44cc21f2
Семененко Надежда <tn@impsa.ru>
2023-03-21 11:14:59 +0300

    delete cart page

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
         <div className="content">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/cart" element={<Cart />} />
+            {/* <Route path="/cart" element={<Cart />} /> */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </div>
